0ff02bb4e7adc6f576fde66e99ddd3aca05df41f
Samyak Bambole <64409597+samyakbambole@users.noreply.github.com>
2020-05-07 13:36:08 +0530

    Adding Calculator

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+# Building a calculator Program
+# 7/5/2020
+
+num1 = (input("Enter First Num: "))
+num2 = (input("Enter Second Num: "))
+
+result = float(num1) + float(num2) # Sorry This calculator can only add numbers 
+
+print(result)
